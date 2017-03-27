36e49c9feac669c2151e4a81281f9a0d3d592392
Gloria Friesen and Shane Stafford <shane@>
2017-03-27 15:37:27 -0700

    update with select form for assigning task to category

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,7 +12,7 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      // model.put("tasks", request.session().attribute("tasks"));
+      model.put("categories", Category.all());
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
